1100024383914ec753779ee140c56efe562228fa
eduardocsouza <dudusoja@gmail.com>
2019-09-19 16:01:16 -0300

    Aula

---
+++ src/Exercicio.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Exercicio {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		/*
+		 Exerício-1
+		System.out.print("Informe a senha: ");
+		int senha = sc.nextInt();
+		
+		while(senha != 2002) {			
+			System.out.println("Senha inválida");
+			System.out.print("Informe a senha: ");
+			senha = sc.nextInt();
+			
+		}
+		
+		System.out.println("Acesso Permitido.");
+		*/
+		
+		/*Exercício
+		int x = 0;
+		int y = 0;
+		System.out.print("Informe o valor de X e Y: ");
+		x = sc.nextInt();
+		y = sc.nextInt();
+		while(x != 0 && y!=0) {
+			if(x > 0 && y > 0) {
+				System.out.println("Primeiro");
+			}
+			else if(x > 0  && y < 0) {
+				System.out.println("Quarto");
+			}
+			else if(x < 0 && y < 0) {
+				System.out.println("Terceiro");
+			}
+			else {
+				System.out.println("Segundo");
+			}
+			System.out.print("Informe o valor de X e Y: ");
+			x = sc.nextInt();
+			y = sc.nextInt();
+		}
+		*/
+		
+		
+		sc.close();
+	}
+
+}
---
+++ src/ExercicioFor.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class ExercicioFor {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		/*
+		 Exercício-1
+		System.out.println("Informe um valor de 1 até 1000: ");
+		int x = sc.nextInt();
+		for(int i=1; i<x; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}	
+		}
+		
+		Exercício-2
+		System.out.println("Informe um número: ");
+		int n = sc.nextInt();
+		int in = 0;
+		int out = 0;
+		for(int i=1; i<=n; i++) {
+			int x = sc.nextInt();
+			if(x>0 && x<=20) {
+				in += 1;
+			}else {
+				out += 1;
+			}
+			
+		}
+		System.out.println(in + " in");
+		System.out.println(out + " out");
+		*/
+		//exercício-3
+		
+		
+		sc.close();
+	}
+}
